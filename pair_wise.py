c820a9f3efa4fab465a9c2477cf93f21fbd9a254
109598087 <t109598087@ntut.org.tw>
2021-05-23 15:49:46 +0800

    create post ISP and CFG ok

--- pair_wise.py
+++ pair_wise.py
@@ -3,9 +3,10 @@ import pandas as pd
 
 parameters = [
     ["A1", "A2", "A3"],
-    # ["B1", "B2", "B3", "B4"],
-
-
+    ["B1"],
+    ["C1", "C2", "C3"],
+    ["D1", "D2", "D3"],
+    ["E1", "E2", "E3"],
 ]
 
 for i, pairs in enumerate(AllPairs(parameters)):
@@ -13,19 +14,19 @@ for i, pairs in enumerate(AllPairs(parameters)):
 
 i_list = list()
 A_list = list()
-# B_list = list()
-# C_list = list()
-# D_list = list()
-# E_list = list()
+B_list = list()
+C_list = list()
+D_list = list()
+E_list = list()
 # F_list = list()
 # G_list = list()
 for i, pairs in enumerate(AllPairs(parameters)):
     i_list.append(i + 1)
     A_list.append(pairs[0])
-    # B_list.append(pairs[1])
-    # C_list.append(pairs[2])
-    # D_list.append(pairs[2])
-    # E_list.append(pairs[2])
+    B_list.append(pairs[1])
+    C_list.append(pairs[2])
+    D_list.append(pairs[2])
+    E_list.append(pairs[2])
     # F_list.append(pairs[2])
     # G_list.append(pairs[2])
 
@@ -33,10 +34,10 @@ for i, pairs in enumerate(AllPairs(parameters)):
 df = pd.DataFrame({
     'pair wise': i_list,
     'A': A_list,
-    # 'B': B_list,
-    # 'C': C_list,
-    # 'D': C_list,
-    # 'E': C_list,
+    'B': B_list,
+    'C': C_list,
+    'D': C_list,
+    'E': C_list,
     # 'F': C_list,
     # 'G': C_list,
 })
