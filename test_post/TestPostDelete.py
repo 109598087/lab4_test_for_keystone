d2f0b22f706253b8a1d975fbb3707a9a4ae4a9de
109598087 <t109598087@ntut.org.tw>
2021-05-25 20:35:52 +0800

    delete post ok

--- test_post/TestPostDelete.py
+++ test_post/TestPostDelete.py
@@ -1,18 +1,43 @@
 from selenium import webdriver
 import unittest
 
+from selenium.common.exceptions import NoSuchElementException
+
+from keywords.wait_until_is_visible import wait_until_home_page_is_visible
+from test_post.TestPostCreate import sign_in_as_admin, go_to_posts_page_from_admin_ui_page, create_a_post, \
+    delete_a_post, sign_out, go_back_to_home_page_from_sign_in_page
+
 
 class TestPostDelete(unittest.TestCase):
+    driver = None
+
     @classmethod
     def setUpClass(cls) -> None:
-        pass
+        cls.driver = webdriver.Chrome('../chromedriver.exe')
 
     def setUp(self) -> None:
-        pass
+        self.driver.get('http://127.0.0.1:3000/')
+        self.driver.maximize_window()
+        wait_until_home_page_is_visible(self)
+        sign_in_as_admin(self)
+        go_to_posts_page_from_admin_ui_page(self)
+        post_name = 'abc'
+        create_a_post(self, post_name)
+
+    def test_delete_a_post(self):
+        post_name = 'abc'
+        self.driver.back()  # todo: 要移除?
+        delete_a_post(self, post_name)
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_link_text('This Post was created by selenium #Edit')
+        print('test_delete_a_post ok')
+        # todo: add print(message)!!!
 
     def tearDown(self) -> None:
-        pass
+        sign_out(self)
+        go_back_to_home_page_from_sign_in_page(self)
 
     @classmethod
     def tearDownClass(cls) -> None:
-        pass
+        cls.driver.close()
+        cls.driver.quit()
