6ea808c8a0ab2a9d3bb94729ae39cd99360cd365
109598087 <t109598087@ntut.org.tw>
2021-05-26 23:49:11 +0800

    delete_comment ok

---
+++ test_comment/TestCommentDelete.py
@@ -0,0 +1,127 @@
+import time
+
+from selenium import webdriver
+import unittest
+
+from selenium.common.exceptions import NoSuchElementException
+
+from keywords.wait_until_is_visible import wait_until_home_page_is_visible, wait_until_comments_page_is_visible, \
+    wait_until_delete_warning_dialog, wait_until_edit_comment_page_is_visible
+from test_comment.TestCommentCreate import go_to_admin_ui_page_from_comments_page, \
+    go_to_comments_page_from_admin_ui_page, create_a_comment, verify_comments_page_have_comment
+from test_post.TestPostCreate import sign_in_as_admin, sign_out, go_back_to_home_page_from_sign_in_page, \
+    go_to_posts_page_from_admin_ui_page, create_a_post
+
+
+def click_cancel_button(self):
+    self.driver.find_element_by_xpath('//*[contains(text(), "Cancel") and @data-button-type="cancel"]').click()
+
+
+#############################################################################
+def delete_a_comment(self, comment_id):
+    self.driver.find_element_by_xpath('//*[@href="/keystone/post-comments/' + comment_id + '"]').click()
+    wait_until_edit_comment_page_is_visible(self)
+    self.driver.find_element_by_xpath('//*[contains(@data-button, "delete")]').click()
+    wait_until_delete_warning_dialog(self)
+    self.driver.find_element_by_xpath(
+        '//*[contains(@data-button-type, "confirm") and contains(text(), "Delete")]').click()
+    wait_until_comments_page_is_visible(self)
+
+
+class TestPostCreate(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome('../chromedriver.exe')
+
+    def setUp(self) -> None:
+        self.driver.get('http://127.0.0.1:3000/')
+        self.driver.maximize_window()
+        wait_until_home_page_is_visible(self)
+        sign_in_as_admin(self)
+
+    def test_delete_comment_with_ISP_input1(self):
+        # setup
+        go_to_posts_page_from_admin_ui_page(self)
+        post_name = 'post_name'
+        create_a_post(self, post_name)
+        go_to_admin_ui_page_from_comments_page(self)
+        go_to_comments_page_from_admin_ui_page(self)
+        comment_author = 'Demo User'
+        create_a_comment(self, comment_author, post_name)
+        self.driver.back()  # todo: back?
+
+        # delete comment
+        comment_id = self.driver.find_element_by_xpath('//*[contains(@href, "/keystone/post-comments/")]').text
+        delete_a_comment(self, comment_id)
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_link_text(comment_id)
+        print('test_delete_comment ok')
+
+    def test_delete_comment_with_ISP_input2(self):
+        # setup
+        go_to_posts_page_from_admin_ui_page(self)
+        post_name = 'post_name'
+        create_a_post(self, post_name)
+        go_to_admin_ui_page_from_comments_page(self)
+        go_to_comments_page_from_admin_ui_page(self)
+        comment_author = 'Demo User'
+        create_a_comment(self, comment_author, post_name)
+        self.driver.back()  # todo: back?
+
+        # delete comment
+        comment_id = self.driver.find_element_by_xpath('//*[contains(@href, "/keystone/post-comments/")]').text
+        delete_a_comment(self, comment_id)
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_link_text(comment_id)
+        print('test_delete_comment ok')
+
+    def test_delete_comment_with_ISP_input3(self):
+        # setup
+        go_to_posts_page_from_admin_ui_page(self)
+        post_name = 'post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name'
+        create_a_post(self, post_name)
+        go_to_admin_ui_page_from_comments_page(self)
+        go_to_comments_page_from_admin_ui_page(self)
+        comment_author = 'Demo User'
+        create_a_comment(self, comment_author, post_name)
+        self.driver.back()  # todo: back?
+
+        # delete comment
+        comment_id = self.driver.find_element_by_xpath('//*[contains(@href, "/keystone/post-comments/")]').text
+        delete_a_comment(self, comment_id)
+        with self.assertRaises(NoSuchElementException):
+            self.driver.find_element_by_link_text(comment_id)
+        print('test_delete_comment ok')
+
+    def test_delete_comment_with_cancel(self):
+        # setup
+        go_to_posts_page_from_admin_ui_page(self)
+        post_name = 'post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name_post_name'
+        create_a_post(self, post_name)
+        go_to_admin_ui_page_from_comments_page(self)
+        go_to_comments_page_from_admin_ui_page(self)
+        comment_author = 'Demo User'
+        create_a_comment(self, comment_author, post_name)
+        self.driver.back()  # todo: back?
+
+        # delete comment
+        comment_id = self.driver.find_element_by_xpath('//*[contains(@href, "/keystone/post-comments/")]').text
+        self.driver.find_element_by_xpath('//*[@href="/keystone/post-comments/' + comment_id + '"]').click()
+        wait_until_edit_comment_page_is_visible(self)
+        self.driver.find_element_by_xpath('//*[contains(@data-button, "delete")]').click()
+        wait_until_delete_warning_dialog(self)
+        click_cancel_button(self)
+        self.driver.back()
+        verify_comments_page_have_comment(self, comment_id)
+
+    def tearDown(self) -> None:
+        # todo: delete all post?
+        sign_out(self)
+        go_back_to_home_page_from_sign_in_page(self)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
